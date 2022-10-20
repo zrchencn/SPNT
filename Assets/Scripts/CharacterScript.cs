202e7307e5fd4aa89cbacdb785a053854de19229
yoorobert <yoo.ro@northeastern.edu>
2022-10-20 18:40:41 -0400

    Added Crouching Function and Collision Raycast

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -7,33 +7,17 @@ public class CharacterScript : MonoBehaviour
     [SerializeField]
     [Tooltip("Insert Character Controller")]
     private CharacterController controller;
-    [SerializeField]
-    private Animator playerAnimator;
 
-    [SerializeField]
     private Vector3 velocity;
-    [SerializeField]
     private bool grounded;
-    [SerializeField]
-    private float groundCastDist = 1.4f;
-    [SerializeField]
-    private float gravity = -20f;
-    [SerializeField]
-    private float forwardRunSpeed = 7f;
-    [SerializeField]
-    private float sidestepSpeed = 7f;
-    [SerializeField]
-    private float jumpHeight = 100f;
-
-    private enum PlayerState
-    {
-        WALKING,
-        JUMPING,
-        SLIDING,
-        DEAD
-    }
-
-    private PlayerState current_state = PlayerState.WALKING;
+    private bool hit;
+    private float gravity = -9.8f;
+    private float groundCastDist = 0.5f;
+    public float forwardRunSpeed = 50f;
+    public float sidestepSpeed = 30f;
+    public float jumpHeight = 30f;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -46,64 +30,33 @@ public class CharacterScript : MonoBehaviour
         // Grounded
         Transform playerTransform = transform;
         grounded = Physics.Raycast(playerTransform.position, Vector3.down, groundCastDist);
-        
-        // Debugging
-        if (Physics.Raycast(playerTransform.position, Vector3.down, groundCastDist))
+
+        // Ground Movement
+        float x = Input.GetAxis("Horizontal");
+        Vector3 movement = (playerTransform.right * x) + (playerTransform.forward * 1);
+        controller.Move(movement * (forwardRunSpeed * Time.deltaTime));
+
+        // Jumping
+        if (Input.GetButton("Jump") && grounded)
         {
-            Debug.DrawRay(playerTransform.position, Vector3.down, Color.blue);
+            velocity.y = Mathf.Sqrt(jumpHeight);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        
+        // Crouching
+        if (Input.GetKey(KeyCode.LeftShift) && grounded)
+        {
+            controller.height = 2.75f;
         }
         else
         {
-            Debug.DrawRay(playerTransform.position, Vector3.down, Color.red);
+            controller.height = 5.5f;
         }
-        velocity.y += gravity * Time.deltaTime;
+        
+        // Object Collision
+        hit = Physics.Raycast(playerTransform.position, Vector3.forward, 0.1f);
 
-        switch (current_state)
-        {
-            case PlayerState.WALKING:
-                move();
-                playerAnimator.SetBool("is_jumping", false);
-                // Jumping
-                if (Input.GetButtonDown("Jump") && grounded)
-                {
-                    current_state = PlayerState.JUMPING;
-                    velocity.y = Mathf.Sqrt(jumpHeight);
-                }
-                break;
-            case PlayerState.JUMPING:
-                move();
-                playerAnimator.SetBool("is_jumping", true);
-                if (grounded)
-                {
-                    current_state = PlayerState.WALKING;
-                }
-                break;
-            case PlayerState.SLIDING:
-                break;
-            case PlayerState.DEAD:
-                playerAnimator.SetBool("is_jumping", false);
-                playerAnimator.SetBool("is_dead", true);
-                break;
-            
-        }
-        controller.Move(velocity * Time.deltaTime);
-    }
-
-    void move()
-    {
-        // Movement
-        float x = Input.GetAxisRaw("Horizontal");
-        velocity.z = forwardRunSpeed;
-        velocity.x = x * sidestepSpeed;
-    }
-    void OnCollisionEnter(Collision collision)
-    {            current_state = PlayerState.DEAD;
-
-        if (collision.gameObject.CompareTag("harmful"))
-        {
-            current_state = PlayerState.DEAD;
-        }
     }
 }
-
-
