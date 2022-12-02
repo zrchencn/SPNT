6b01bb845872ae3755480d14887b2e3c6284db40
Ruochen Zhuang <zrchencn@gmail.com>
2022-12-02 00:06:18 -0500

    miner changes to menu, add highest score to start menu

--- Assets/Scripts/StartSceneManager.cs
+++ Assets/Scripts/StartSceneManager.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class StartSceneManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Label highestScoreLabel;
     void Start()
     {
-        
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        highestScoreLabel = root.Q<Label>("HighestScore");
+        highestScoreLabel.text = "Highest Score: " + MainManager.Instance.HighScore.ToString("0");
     }
 
     // Update is called once per frame
